904f869c4b93f68b15a5e71b2af354038a26115c
dxmv <dima@example.com>
2022-10-22 11:54:45 +0200

    Backend Types

---
+++ client/src/types.ts
@@ -0,0 +1,31 @@
+export interface User {
+	userId: number;
+	username: string;
+	email: string;
+	password: string;
+	image: string;
+	bio: string;
+	posts: Post[];
+	followedBy: User[];
+	following: User[];
+	likedPosts: Post[];
+	comments: Comment[];
+	likedComments: Comment[];
+}
+
+export interface Post {
+	postId: number;
+	image: string;
+	caption: string;
+	user: User;
+	likedBy: User[];
+	comments: Comment[];
+}
+
+export interface Comment {
+	commentId: number;
+	text: string;
+	post: Post;
+	user: User;
+	likedBy: User[];
+}
