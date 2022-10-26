4e29b6a2a5b556deb535f5d4b18f470a5ab61354
dxmv <dimitirjestepanovic1@gmail.com>
2022-10-26 17:56:45 +0200

    Register second page

--- client/src/pages/Register/SecondPage.tsx
+++ client/src/pages/Register/SecondPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RegisterState } from "./registerTypes";
 
 export default function SecondPage({
@@ -12,17 +12,41 @@ export default function SecondPage({
 	user: RegisterState;
 	setUser: React.Dispatch<React.SetStateAction<RegisterState>>;
 }) {
+	const [error, setError] = useState<string>("");
+
+	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const text = e.target.value;
+		if (text.length >= 50) {
+			setError("Maximum length is 50");
+		} else {
+			setError("");
+			setUser(prev => {
+				return { ...prev, bio: text };
+			});
+		}
+	};
+
+	const handleNext = () => {
+		if (error !== "") {
+			return;
+		}
+		nextPage();
+	};
+
 	return (
 		<>
 			<textarea
-				className="text-lg w-4/5 shadow-lg border-2 mb-6 p-3 rounded-lg"
+				className="text-lg w-4/5 shadow-lg border-2 mb-2 p-3 rounded-lg"
 				placeholder={"Tell us about yourself..."}
-				style={{ resize: "none" }}
+				style={{ resize: "none", borderColor: error === "" ? "black" : "red" }}
 				rows={10}
+				defaultValue={user.bio}
+				onChange={handleChange}
 			/>
+			<p className="text-red-600 mb-6">{error}</p>
 			<button
 				className="mb-6 bg-orange-400 w-1/4 p-3 font-bold text-white rounded-lg text-lg"
-				onClick={nextPage}
+				onClick={handleNext}
 			>
 				NEXT
 			</button>
